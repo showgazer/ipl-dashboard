5ee07b6cb5145b4edb9403a72dd75ddbe09486d2
p.shirchkov <p.shirchkov@infomaximum-llc.ru>
2021-04-28 11:21:39 +0300

    UI elements for team page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import {TeamPage} from "./pages/TeamPage";
+import {MatchPage} from "./pages/MatchPage";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-        <TeamPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/teams/:teamName/matches/:year">
+                        <MatchPage/>
+                    </Route>
+                    <Route path="/teams/:teamName">
+                        <TeamPage/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
